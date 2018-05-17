da52ea02e989fa3885d96f8955b66507a28d86a5
fenggang <fenggang@bluedeer.com.cn>
2018-05-17 15:57:01 +0800

    Site updated: 2018-05-17 15:56:59

---
+++ js/music.js
@@ -0,0 +1,21 @@
+function play(){
+    $('#music')[0].play();
+    $('.music_btn').addClass('music_anim');
+  };
+
+  function pause(){
+    $('#music')[0].pause();
+    $('.music_btn').removeClass('music_anim');
+  };
+  $('.music_btn').on('click',function(e){
+		e.stopPropagation();
+		e.preventDefault();
+		if($(this).hasClass('music_anim')){
+			pause();
+		}
+		else{
+			play();
+		}
+		return false;
+
+	});
